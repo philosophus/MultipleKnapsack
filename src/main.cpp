14ca5624eec52ad1249986732260589766fee972
Florian Dahms <dahms@or.rwth-aachen.de>
2012-06-29 16:19:12 +0200

    Initial commit.

---
+++ src/main.cpp
@@ -0,0 +1,100 @@
+/* 
+ * File:   main.cpp
+ * Author: florian
+ *
+ * Created on June 29, 2012, 3:42 PM
+ */
+
+#include <cstdlib>
+
+/* scip includes */
+#include "objscip/objscip.h"
+#include "objscip/objscipdefplugins.h"
+
+extern "C" {
+
+//#include "scip/scip.h"
+#include "scip/scipshell.h"
+//#include "scip/scipdefplugins.h"
+
+#include "branch_ryanfoster.h"
+#include "cons_samediff.h"
+#include "pricer_binpacking.h"
+#include "reader_bpa.h"
+    
+}
+
+using namespace std;
+
+/** creates a SCIP instance with default plugins, evaluates command line parameters, runs SCIP appropriately,
+ *  and frees the SCIP instance
+ */
+static
+SCIP_RETCODE runShell(
+   int                        argc,               /**< number of shell parameters */
+   char**                     argv,               /**< array with shell parameters */
+   const char*                defaultsetname      /**< name of default settings file */
+   )
+{
+   SCIP* scip = NULL;
+
+   /*********
+    * Setup *
+    *********/
+
+   /* initialize SCIP */
+   SCIP_CALL( SCIPcreate(&scip) );
+   
+   /* include binpacking reader */
+   SCIP_CALL( SCIPincludeReaderBpa(scip) );
+
+   /* include binpacking branching and branching data */
+   SCIP_CALL( SCIPincludeBranchruleRyanFoster(scip) );
+   SCIP_CALL( SCIPincludeConshdlrSamediff(scip) );
+   
+  /* include binpacking pricer  */
+   SCIP_CALL( SCIPincludePricerBinpacking(scip) );
+
+   /* include default SCIP plugins */
+   SCIP_CALL( SCIPincludeDefaultPlugins(scip) );
+ 
+   /* for column generation instances, disable restarts */
+   SCIP_CALL( SCIPsetIntParam(scip,"presolving/maxrestarts",0) );
+
+   /* turn off all separation algorithms */
+   SCIP_CALL( SCIPsetSeparating(scip, SCIP_PARAMSETTING_OFF, TRUE) );
+   
+   /**********************************
+    * Process command line arguments *
+    **********************************/
+   SCIP_CALL( SCIPprocessShellArguments(scip, argc, argv, defaultsetname) );
+
+   /********************
+    * Deinitialization *
+    ********************/
+
+   SCIP_CALL( SCIPfree(&scip) );
+
+   BMScheckEmptyMemory();
+
+   return SCIP_OKAY;
+}
+
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+   SCIP_RETCODE retcode;
+
+   retcode = runShell(argc, argv, "scip.set");
+   if( retcode != SCIP_OKAY )
+   {
+      SCIPprintError(retcode, stderr);
+      return -1;
+   }
+
+   return 0;
+}
+
+
